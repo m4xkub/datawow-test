793498febd3a38d22da7b7e81ca8f5127dad996b
max <153589599+m4xkub@users.noreply.github.com>
2025-06-08 19:20:21 +0700

    update stat component

--- concert-frontend/src/app/components/statComponent.tsx
+++ concert-frontend/src/app/components/statComponent.tsx
@@ -1,27 +1,52 @@
+"use client";
+import { useEffect, useState } from "react";
 import { FaUser, FaMedal, FaTimesCircle } from "react-icons/fa";
+import GetNumReserve from "../api/getNumReserve";
+import GetNumCancel from "../api/getNumCancel";
+import GetTotalSeats from "../api/getTotalSeats";
 
 export default function StatComponent() {
+  const [reserve, setReserve] = useState(0);
+  const [cancel, setCancel] = useState(0);
+  const [seats, setSeats] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   const cards = [
     {
       title: "Total of seats",
-      value: 500,
+      value: seats,
       icon: <FaUser size={30} />,
       bg: "bg-sky-800",
     },
     {
       title: "Reserve",
-      value: 120,
+      value: reserve,
       icon: <FaMedal size={30} />,
       bg: "bg-teal-600",
     },
     {
       title: "Cancel",
-      value: 12,
+      value: cancel,
       icon: <FaTimesCircle size={30} />,
       bg: "bg-red-400",
     },
   ];
 
+  useEffect(() => {
+    const fetchStat = async () => {
+      const NumReserve = await GetNumReserve();
+      const NumCancel = await GetNumCancel();
+      const totalSeats = await GetTotalSeats();
+      setReserve(NumReserve.result);
+      setCancel(NumCancel.result);
+      setSeats(totalSeats.result);
+      setLoading(false);
+    };
+
+    fetchStat();
+  }, []);
+
+  if (loading) return null;
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
       {cards.map((card) => (
