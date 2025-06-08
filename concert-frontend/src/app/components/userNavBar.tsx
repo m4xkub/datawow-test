f193e42fe040dd4a71152cbdfcf44e18e5aba785
max <153589599+m4xkub@users.noreply.github.com>
2025-06-08 20:55:40 +0700

    add history for user

--- concert-frontend/src/app/components/userNavBar.tsx
+++ concert-frontend/src/app/components/userNavBar.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { redirect } from "next/navigation";
-import { FaSignOutAlt } from "react-icons/fa";
+import { useState } from "react";
+import { FaHome, FaSignOutAlt } from "react-icons/fa";
+import { FaBoxArchive } from "react-icons/fa6";
 
 export default function UserNavBar() {
+  const [activePage, setActivePage] = useState("Home");
+
   function handleLogout() {
     localStorage.setItem("token", "");
     localStorage.setItem("role", "");
@@ -12,6 +16,30 @@ export default function UserNavBar() {
       <div className="bg-white text-black font-bold text-3xl py-6 px-4 mt-8 max-sm:hidden">
         User
       </div>
+      <button
+        className={`flex items-center gap-2 text-black py-4 px-4 mx-2 mt-2 rounded overflow-hidden max-sm:m-0 ${
+          activePage == "Home" ? "bg-blue-100 font-semibold" : "bg-white"
+        }`}
+        onClick={() => {
+          setActivePage("Home");
+          redirect("/user");
+        }}
+      >
+        <FaHome />
+        <span>Home</span>
+      </button>
+      <button
+        className={`flex items-center gap-2 text-black py-4 px-4 mx-2 mt-2 rounded overflow-hidden max-sm:m-0 ${
+          activePage == "History" ? "bg-blue-100 font-semibold" : "bg-white"
+        }`}
+        onClick={() => {
+          setActivePage("History");
+          redirect("/user/history");
+        }}
+      >
+        <FaBoxArchive />
+        <span>History</span>
+      </button>
       <div className="flex-grow" />
       <button
         className="flex items-center gap-2 bg-white text-black py-4 px-4 mx-2 mb-8 rounded overflow-hidden max-sm:m-0"
