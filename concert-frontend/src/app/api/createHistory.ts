c7f7a7280d024d6d2086d0090263bf6a2822ae6d
max <153589599+m4xkub@users.noreply.github.com>
2025-06-08 19:20:07 +0700

    create history api

---
+++ concert-frontend/src/app/api/createHistory.ts
@@ -0,0 +1,31 @@
+import { UserAction } from "../config/userAction";
+
+export default async function CreateHistory(
+  concertName: string,
+  action: UserAction
+) {
+  const backendUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+  console.log("url", backendUri);
+  if (!backendUri) {
+    throw new Error("BACKEND_URI is not defined");
+  }
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(backendUri + "/history/create", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ concertName, action }),
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  const { message, result } = await res.json();
+
+  return { message, result };
+}
