22894a176d552665d5af3bee64e2d70957f8f12f
max <153589599+m4xkub@users.noreply.github.com>
2025-06-08 19:19:23 +0700

    api about reserve and cancel

---
+++ concert-frontend/src/app/api/isReserved.ts
@@ -0,0 +1,26 @@
+export default async function IsReserved(concertId: string) {
+  const backendUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+  console.log("url", backendUri);
+  if (!backendUri) {
+    throw new Error("BACKEND_URI is not defined");
+  }
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(backendUri + "/concert/isReserved", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ concertId }),
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  const { result } = await res.json();
+
+  return { result };
+}
